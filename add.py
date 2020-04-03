2b2e5b10086c882183d9f9a00640ba89d6d1ce6b
sidd732 <nashikkarsid@gmail.com>
2020-04-03 23:40:01 +0530

    add.py

--- add.py
+++ add.py
@@ -1,6 +1,18 @@
-a=int(input("enter first integer: "))
-b=int(input("enter second number: "))
+while True:
+    n=int(input("Enter the number of elements you want to add: "))
+    a=input("Do you want to re-enter the total numbers (yes/no): ")
+    if a=="yes":
+        continue
+    elif a=="no":
+        break
+    else:
+        print("command not understood hence continueing with erlier  value")
+        break
+    
 
-c=a+b
+lst=[]
+for i in range(n):
+    numbers=int(input("enter numbers: "))
+    lst.append(numbers)
 
-print("addition of two number is: ", c)
+print("sum is: ",sum(lst))
